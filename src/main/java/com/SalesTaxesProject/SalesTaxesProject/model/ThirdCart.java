6385100e6ce63ed139ffb50120c97869f972ea0a
nico5310 <76699524+nico5310@users.noreply.github.com>
2024-07-04 12:43:54 +0200

    Refactor class

---
+++ src/main/java/com/SalesTaxesProject/SalesTaxesProject/model/ThirdCart.java
@@ -0,0 +1,20 @@
+package com.SalesTaxesProject.SalesTaxesProject.model;
+
+import com.SalesTaxesProject.SalesTaxesProject.UserAction;
+import com.SalesTaxesProject.SalesTaxesProject.database.Database;
+import com.SalesTaxesProject.SalesTaxesProject.service.CartService;
+import com.SalesTaxesProject.SalesTaxesProject.service.MenuSelectorService;
+
+public class ThirdCart implements UserAction {
+
+    private Database database = new Database();
+    private CartService cartService = new CartService();
+
+    @Override
+    public void execute() {
+        database.initDatabase();
+        cartService.displayCartSelection(database.getThirdCart());
+        MenuSelectorService.loadMenu();
+    }
+
+}
---
+++ src/main/java/com/SalesTaxesProject/SalesTaxesProject/service/MenuSelectorService.java
@@ -0,0 +1,37 @@
+package com.SalesTaxesProject.SalesTaxesProject.service;
+
+import com.SalesTaxesProject.SalesTaxesProject.UserAction;
+import com.SalesTaxesProject.SalesTaxesProject.UserActionFactory;
+import com.SalesTaxesProject.SalesTaxesProject.database.Database;
+import com.SalesTaxesProject.SalesTaxesProject.util.InputReaderUtil;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MenuSelectorService {
+
+    private static InputReaderUtil inputReaderUtil = new InputReaderUtil();
+    private static UserActionFactory userActionFactory = new UserActionFactory();
+
+
+    private static String messageSelection = "Please enter your cart number:" + "\n" + "1 First Cart / " + "2 Second Cart / " + "3 Third Cart / " + "0 Exit";
+
+    public static void setContinueApp(boolean continueApp) {
+        MenuSelectorService.continueApp = continueApp;
+    }
+
+    private static boolean continueApp = true;
+
+    public static void loadingMenuSelector() {
+        loadMenu();
+        while (continueApp) {
+            int userChoice = inputReaderUtil.readSelection();
+            UserAction action = userActionFactory.action(userChoice);
+            action.execute();
+        }
+    }
+
+    public static void loadMenu() {
+        System.out.println(messageSelection);
+    }
+
+}
